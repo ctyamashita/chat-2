0c9e4fff6a83350f87822533e467c3d5fcc62f68
Celso Yamashita <ctyamashita@hotmail.com>
2023-02-09 02:31:02 +0900

    chat 2.0

---
+++ index.js
@@ -0,0 +1,119 @@
+// const batch = window.location.search.match(/channel=(\d+)/)[1];
+// const user = window.location.search.match(/name=(.+)/)[1];
+
+// let batch = new URL(window.location.href).searchParams.get("channel");
+// let user = new URL(window.location.href).searchParams.get("name");
+
+// document.querySelector('#your-name').innerText = user;
+
+
+const batch = 858; // change to your own batch id
+const baseUrl = `https://wagon-chat.herokuapp.com/${batch}/messages`;
+document.querySelector("h1").innerText = `#${batch}`
+
+if (window.localStorage.getItem('name')) {
+  document.querySelector("#your-name").value = window.localStorage.getItem('name');
+}
+
+// Your turn to code!
+const messageBoard = document.querySelector('#messages');
+
+const buildMsg = (message, name) => {
+  // prevent code injection
+  const content = message.content.replaceAll(/</g, "&lt").replaceAll(/>/g, "&gt");
+
+  const time = new Date(message.created_at);
+  const hours = time.getHours();
+  const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
+  return `<div class="msg d-flex ${name === message.author ? 'justify-content-end' : 'justify-content-start'}">
+            <div class="${name === message.author ? 'bg-warning bubble-right' : 'bg-light bubble-left'} py-2 px-3 my-1 col-8">
+              <div class="msg-container">
+                <p class="m-0 p-0 border-bottom d-flex justify-content-between">
+                  <strong>${message.author}</strong> <span class="date">${hours}:${minutes}</span>
+                </p>
+                <small>
+                  ${content}
+                </small>
+              </div>
+            </div>
+          </div>`
+}
+
+let lastMsg
+
+const refresh = () => {
+  fetch(baseUrl)
+  .then(response => response.json())
+  .then((data) => {
+      const name = document.querySelector("#your-name").value;
+      window.localStorage.setItem('name', name)
+      if (lastMsg) {
+        data.messages.forEach((message) => {
+          if (message.id > lastMsg.id) {
+
+            const msgContent = buildMsg(message, name)
+            messageBoard.insertAdjacentHTML("beforeend", msgContent);
+            if (document.querySelector('#messages').lastElementChild && lastMsg.id != message.id) {
+              document.querySelector('#messages').lastElementChild.scrollIntoView();
+            }
+            lastMsg = message
+          }
+        });
+
+      } else {
+        messageBoard.innerHTML = "";
+        data.messages.forEach((message) => {
+
+            const msgContent = buildMsg(message, name)
+            messageBoard.insertAdjacentHTML("beforeend", msgContent);
+            lastMsg = message
+        });
+      }
+
+
+    });
+};
+
+// btnRefresh.addEventListener('click', refresh);
+
+const checkName = (inputName) => {
+  if (inputName.value) {
+    inputName.classList.add('bg-dark');
+    inputName.classList.add('text-white');
+    inputName.classList.remove('bg-danger');
+  } else {
+    inputName.classList.add('bg-danger');
+    inputName.classList.remove('bg-dark');
+    inputName.classList.remove('text-white');
+  }
+}
+
+const sendMessage = (event) => {
+  event.preventDefault();
+  const inputName = document.querySelector("#your-name");
+  const msgInput = document.querySelector('#your-message');
+
+  checkName(inputName);
+
+  if (event.key === "Enter" && msgInput === document.activeElement) {
+    if (!inputName.value) {
+      alert('Please add a username.')
+    } else {
+      const yourMessage = document.querySelector('#your-message').value;
+      const yourName = document.querySelector('#your-name').value;
+      fetch(baseUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ author: yourName, content: yourMessage }),
+      });
+      document.querySelector('#your-message').value = ''
+      refresh();
+    }
+  }
+};
+
+const form = document.querySelector("#comment-form");
+form.addEventListener("keyup", sendMessage);
+
+
+setInterval(() => { refresh(); }, 1000);
