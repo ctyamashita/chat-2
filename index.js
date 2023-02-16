0f8e7f94d26c775cb5053e838df7e72c65321fae
Celso Yamashita <ctyamashita@hotmail.com>
2023-02-16 23:48:56 +0900

    refactoring code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ while (channel === '' || channel === null || !Number.isInteger(Number(channel)))
 window.localStorage.setItem('channel', channel)
 document.querySelector("#channel").value = channel
 
-const baseUrl = `https://wagon-chat.herokuapp.com/${channel}/messages`;
+let baseUrl = `https://wagon-chat.herokuapp.com/${channel}/messages`;
 
 const inputName = document.querySelector("#your-name")
 
@@ -23,6 +23,70 @@ if (!window.localStorage.getItem('validName') || window.localStorage.getItem('va
 } else {
   chatHeader.classList.remove('red');
 }
+
+const buildLine = (messageContent) => {
+  return `<p class="msg-txt">${generateIfLink(messageContent)}</p>`
+}
+
+
+const buildMsg = (message, name) => {
+  // preventing code injection
+  let content = message.content
+    .replaceAll(/</g, "&lt").replaceAll(/>/g, "&gt")
+
+    content =  generateIfLink(content)
+
+  const time = new Date(message.created_at);
+  const hours = time.getHours();
+  const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
+  return `<div class="${name === message.author ? 'bubble-container-right' : 'bubble-container-left'}">
+            <div class="${name === message.author ? 'bubble right' : 'bubble left'}">
+              ${name === message.author ? '' : `<div class="avatar"><img src="https://github.com/${message.author}.png"/></div>`}
+              <div class="msg-container">
+                <p class="msg-head">
+                  <strong>${message.author}</strong> <span class="date">${hours}:${minutes}</span>
+                </p>
+                ${buildLine(message.content)}
+              </div>
+            </div>
+          </div>`
+}
+
+let lastMsg
+
+const refresh = () => {
+  fetch(baseUrl)
+  .then(response => response.json())
+  .then((data) => {
+      const name = window.localStorage.getItem('name');
+      if (!lastMsg) {
+        messageBoard.innerHTML = "";
+        lastMsg = data.messages[0]
+        if (lastMsg) {
+          const msg = buildMsg(lastMsg, name);
+          messageBoard.insertAdjacentHTML("beforeend", msg);
+        }
+      }
+
+      data.messages.forEach((message) => {
+        if (message.id > lastMsg.id) {
+          let msgContent
+          if (message.author === lastMsg.author) {
+            msgContent = buildLine(message.content)
+            const lastBubble = messageBoard.lastElementChild.firstElementChild.lastElementChild
+            lastBubble.insertAdjacentHTML("beforeend", msgContent);
+          } else {
+            msgContent = buildMsg(message, name);
+            messageBoard.insertAdjacentHTML("beforeend", msgContent);
+          }
+          if (document.querySelector('#messages').lastElementChild && lastMsg.id != message.id) {
+            document.querySelector('#messages').lastElementChild.scrollIntoView();
+          }
+          lastMsg = message
+        }
+      });
+    });
+};
 
 const areInputsValid = () => {
   if (document.querySelector('#user')) document.querySelector('#user').remove()
@@ -47,14 +111,30 @@ const areInputsValid = () => {
 
   const channelInput = document.querySelector('#channel')
   const channel = channelInput.value
+
   if (channel === '' || channel === null || !Number.isInteger(Number(channel))) {
     chatHeader.classList.add('red');
     window.localStorage.setItem('validChannel', false)
   } else {
     chatHeader.classList.remove('red');
     window.localStorage.setItem('validChannel', true)
-    window.localStorage.setItem('channel', channel)
   }
+
+  if (window.localStorage.getItem("validName") === "true") {
+    if (window.localStorage.getItem('name') != inputName.value) {
+      lastMsg = false
+      window.localStorage.setItem('name', inputName.value)
+    }
+  }
+
+  if (window.localStorage.getItem("validChannel") === "true") {
+    if (window.localStorage.getItem('channel') != channel) {
+      baseUrl = `https://wagon-chat.herokuapp.com/${channel}/messages`;
+      lastMsg = false
+      window.localStorage.setItem('channel', channel)
+    }
+  }
+
 }
 
 areInputsValid()
@@ -72,109 +152,35 @@ const messageBoard = document.querySelector('#messages');
 const regex = /^https:\/\/(www\.|)([-a-zA-Z0-9@:%._\+~#=]{1,256})\.[a-zA-Z0-9()]{1,6}\S*/
 
 const addYoutubeVideo = (url) => {
+  const idRegexTwo = /https:\/\/youtu.be\/(\w+)/
   const idRegex = /https:\/\/www\.youtube\.com\/watch\?v=(\w+)/
-  if (!!url.match(idRegex)) {
-    const id = url.match(idRegex)[1]
-    return `<iframe width="100%" height="260" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; web-share" allowfullscreen></iframe>`
+  if (!!url.match(idRegex) || url.match(idRegexTwo)[1]) {
+    let id
+    if (!!url.match(idRegex)) id = url.match(idRegex)[1]
+    if (!!url.match(idRegexTwo)) id = url.match(idRegexTwo)[1]
+    return `<iframe width="100%" height="180" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; web-share" allowfullscreen></iframe>`
   }
   return `<a href="${url}" class="link" target=_blank><strong><i class="fa-solid fa-link"></i> ${url.match(regex)[2]}</strong></a>`
 }
 
 const generateIfLink = (word) => {
-  if (regex.test(word) && word.match(regex)[2] && word.match(regex)[2] === 'youtube') {
+  let domain
+  if (word.match(regex)) {
+    domain = word.match(regex)[2]
+  }
+  if (regex.test(word) && domain && domain === 'youtube' || domain === 'youtu') {
     return addYoutubeVideo(word)
   } else {
-    return regex.test(word) ? `<a href="${word}" class="link" target=_blank><strong><i class="fa-solid fa-link"></i> ${word.match(regex)[2]}</strong></a>` : word
+    return regex.test(word) ? `<a href="${word}" class="link" target=_blank><strong><i class="fa-solid fa-link"></i> ${domain}</strong></a>` : word
   }
 }
 
-const buildLine = (messageContent) => {
-  return `<br><small>${generateIfLink(messageContent)}</small>`
-}
 
 
-const buildMsg = (message, name) => {
-  // link regex
 
-  // preventing code injection
-  let content = message.content
-    .replaceAll(/</g, "&lt").replaceAll(/>/g, "&gt")
-
-    content =  generateIfLink(content)
-
-  const time = new Date(message.created_at);
-  const hours = time.getHours();
-  const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
-  return `<div class="msg d-flex ${name === message.author ? 'justify-content-end' : 'justify-content-start'}">
-            <div class="${name === message.author ? 'bubble-right' : 'bubble-left'} py-2 px-3 my-1 col-8 d-flex align-items-center">
-              ${name === message.author ? '' : `<div class="avatar"><img src="https://github.com/${message.author}.png"/></div>`}
-              <div class="msg-container flex-grow-1">
-                <p class="m-0 p-0 border-bottom d-flex justify-content-between">
-                  <strong>${message.author}</strong> <span class="date">${hours}:${minutes}</span>
-                </p>
-                ${buildLine(message.content)}
-              </div>
-            </div>
-          </div>`
-}
-
-let lastMsg
-
-// const mergeMsgs = (messages) => {
-//   const fusedMsgs = []
-//   messages.forEach((message, index) => {
-//     const nextMsg = messages[index + 1]
-//     if (nextMsg && nextMsg.author === message.author) {
-//       nextMsg.content = message.content + '</small><br><small>' + nextMsg.content
-//     } else {
-//       fusedMsgs.push(message)
-//       lastMsg = message
-//     }
-//   });
-//   return fusedMsgs
-// }
-
-
-const refresh = () => {
-  fetch(baseUrl)
-  .then(response => response.json())
-  .then((data) => {
-      const name = document.querySelector("#your-name").value;
-      window.localStorage.setItem('name', name)
-
-      if (lastMsg) {
-        data.messages.forEach((message) => {
-          if (message.id > lastMsg.id) {
-            let msgContent
-            if (message.author === lastMsg.author) {
-              msgContent = buildLine(message.content)
-              messageBoard.lastElementChild.lastElementChild.firstElementChild.insertAdjacentHTML("beforeend", msgContent);
-            } else {
-              msgContent = buildMsg(message, name);
-              messageBoard.insertAdjacentHTML("beforeend", msgContent);
-            }
-            if (document.querySelector('#messages').lastElementChild && lastMsg.id != message.id) {
-              document.querySelector('#messages').lastElementChild.scrollIntoView();
-            }
-            lastMsg = message
-          }
-        });
-
-      } else {
-        messageBoard.innerHTML = "";
-        data.messages.forEach(message => {
-          const msgContent = buildMsg(message, name)
-          messageBoard.insertAdjacentHTML("beforeend", msgContent);
-          lastMsg = message
-        })
-      }
-
-    });
-};
 
 const sendMessage = (event) => {
   event.preventDefault();
-  const inputName = document.querySelector("#your-name");
   const msgInput = document.querySelector('#your-message');
 
   if (event.key === "Enter" && msgInput === document.activeElement) {
@@ -184,7 +190,7 @@ const sendMessage = (event) => {
       alert('Please add a valid channel.')
     } else {
       const yourMessage = msgInput.value;
-      const yourName = inputName.value;
+      const yourName = window.localStorage.getItem('name')
       fetch(baseUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
